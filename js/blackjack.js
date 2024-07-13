0a85db1da1c26b1a451746aec9bfb2df71a01ee2
najbrian <najbrian@gmail.com>
2024-07-13 13:05:57 -0700

    restarting JS code to work from end product to beginning methodology.

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,134 +1,87 @@
-
 /*-------------------------------- Variables --------------------------------*/
-let playerCash = 1000
-
-let deck1 = []
-
-let wager
-
-let cardToRemove
-
-let dealerHand = []
-let playerHand = []
-
-let dealerAceCount = 0
-let playerAceCount = 0
-
+let dealerCount = 0
+let dealerAces = 0
 let dealerHiddenCard
 
-let playerCanHit = true
+let playerCount = 0
+let playerAces = 0
+let playerHit = true
 
+let dealtCardTemp
+
+let deck1
 /*-------------------------------- Constants --------------------------------*/
-
-
-// const dealerTotal = cardsPicked.dealer.reduce((acc, cards) => {
-//   return 
-// })
-
-const playerTotal = Number(player.card1) + Number(player.card2)
-
 
 /*------------------------ Cached Element References ------------------------*/
 const body = document.querySelector('body')
-const startButton = document.querySelector('#start-button');
-const playerCards = document.querySelector('#player');
-const dealerCards = document.querySelector('#dealer');
-const bet = document.querySelector('.bet');
-const playerCashTotal = document.querySelector('#player-cash')
-const helpDirections = document.querySelector('#help-directions')
-const message = document.querySelector('#player-message')
 /*-------------------------------- Functions --------------------------------*/
 
 const init = () => {
-  deck1 = ["dA", "dQ", "dK", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02", "hA", "hQ", "hK", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02", "cA", "cQ", "cK", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02", "sA", "sQ", "sK", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"]
+  deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 }
 
-const placeBet = (evt) => {
-  wager = parseFloat(document.getElementById('bet-placeholder').value);
-  // console.log(typeof parseFloat(evt.target.value));
-  if (typeof wager !== 'number') {
-    return message.innerHTML = "Submit a numeric value to play Blackjack"
+init()
+
+const randomizeCard = () => {
+  let randomIdx = Math.floor(Math.random() * deck1.length)
+  let randomizedCard = deck1.splice(randomIdx, 1)[0]
+  dealtCardTemp = randomizedCard;
+  distributeCard();
   }
-  if (evt.target.id === 'bet-button') {
+
+const distributeCard = () => {
+  dealerHiddenCard = dealtCardTemp;
+  dealerCount += getCardValue(dealerHiddenCard)
+  dealerAces += checkForAces(dealerHiddenCard)
+
+  while (dealerCount<17) {
+    let dealerHitCard = dealtCardTemp
+    let dealerDiv = document.getElementById('dealer');
+    let dealerNewDiv = document.createElement('div');
+
+    dealerNewDiv.classList.add('card', dealerHitCard)
+    dealerDiv.appendChild(dealerNewDiv)
     
-    if(playerCash === 0) {
-      message.innerHTML = "You’ve run out of cash  Better luck next time!"
-      return
-    }
-    if (wager > playerCash) {
-      document.getElementById('bet-button').innerHTML = 'Try Again.'
-      message.innerHTML = "Insufficient Funds. Please place a valid bet"
-      return;
-    } else if (playerCash > wager) {
-      playerCash = playerCash - wager;
-      playerCashTotal.innerHTML = `$${playerCash}`;
-      bet.style.display = 'none';
-      playerCards.style.display = 'block';
-      dealerCards.style.display = 'block';
-      playerCashTotal.style.display = 'block';
-      document.querySelector('#player-buttons').style.display = 'block';
-      init();
-      dealCards();
-    }
-   
-  }
-};
+    dealerCount +=getCardValue(dealerHitCard)
+    dealerAces += checkForAces(dealerHitCard)
 
-const dealCards = () => {
-
-  if (deck1.length > 0) {
-    let randomIdx = Math.floor(Math.random() * deck1.length)
-  
-    let cardPicked = deck1.splice(randomIdx, 1)[0]
-    playerHand.push(cardPicked)
-    render(cardPicked);
-  //   cardPicked = deck1.splice(randomIdx, 1)[0]
-  //   render(cardPicked)
-  //   handInRound[1].push(cardPicked)
-  //   cardPicked = deck1.splice(randomIdx, 1)[0]
-  //   render(cardPicked)
-  //   handInRound[0].push(cardPicked)
-  //   cardPicked = deck1.splice(randomIdx, 1)[0]
-  //   render(cardPicked)
-  //   handInRound[0].push(cardPicked)
-  //   cardPicked = deck1.splice(randomIdx, 1)[0]
-  //   render(cardPicked)
+    // randomizeCard();
   }
+  console.log(dealerCount)
+  console.log(dealerHiddenCard)
 }
 
 
-const render = (cardPicked) => {
-  // 2. Add the class name to display the card picked on deck 2.
-  if(playerHand.length > 1) {
-    playerCards.classList.remove(cardToRemove);
+const getCardValue = (card) => {
+  let cardValue = card.slice(1)
+  let cardNumberValue = cardValue
+  console.log(cardNumberValue);
+
+  if( cardNumberValue === 'A') {
+    return 11;
+  } else if (cardNumberValue ==="K" || cardNumberValue === 'Q' || cardNumberValue === 'J') {
+    return 10;
+  } else {
+    return Number(cardNumberValue)
   }
-  cardToRemove = cardPicked
-  // Apply current picked card deck2's class list. For example, if picked card was "h08", the the deck2El would gain the class "h08", which correlates to a background image of the eight of hearts. 
-  playerCards.classList.add(cardPicked);
+}
+
+const checkForAces = (card) => {
+  if(card.slice(1) === 'A') {
+    return 1;
+  } return 0;
 }
 
 const handleClick = (evt) => {
-  let evtID = evt.target.id
-  if (evtID === 'start-button') {
-    playerCards.style.display = 'none';
-    dealerCards.style.display = 'none';
-    startButton.style.display = 'none';
-    if (bet.style.display = 'none') {
-      bet.style.display = 'block';
-      playerCashTotal.style.display = 'block';
-      playerCashTotal.textContent = `Player Cash: $${playerCash}`;
-    }
-  }
-  if (evtID === 'help-button') {
-    if (helpDirections.style.display === 'none') {
-      helpDirections.style.display = 'block';
-    } else {
-      helpDirections.style.display = 'none';
-    }
-  }
-  placeBet(evt)
-  return
+const button = evt.target.id
+if(button === 'start-button') {
+
+}
+if(button === 'bet-button') {
+  randomizeCard();
+}
+
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
-body.addEventListener('click', handleClick)
+body.addEventListener('click', handleClick);
