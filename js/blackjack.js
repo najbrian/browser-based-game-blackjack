0298e491f1a04cc858b46a89c707c4ea7ba03ae6
najbrian <najbrian@gmail.com>
2024-07-14 10:00:44 -0700

    Started to work on betting functionality - need to still work on saving the bet value and paying out player

--- js/blackjack.js
+++ js/blackjack.js
@@ -5,13 +5,48 @@ let dealerAces = 0
 let playerCount = 0
 let playerAces = 0
 
+let playerCash = 1000
+let playerBet = 0
+
 let dealtCardTemp
 
 let deck1
+
 /*------------------------ Cached Element References ------------------------*/
 const body = document.querySelector('body')
 const message = document.getElementById('player-message')
+
+const dealerCard1 = document.getElementById('dealer-card')
+const dealerCard2 = document.getElementById('dealer-showing-card')
+
+const playerCard1 = document.getElementById('player-card-1')
+const playerCard2 = document.getElementById('player-card-2')
+
+const playerActionButtons = document.getElementById('player-buttons')
+const playerSubmittedBetValue = document.getElementById('bet-placeholder')
+const playerBetButton = document.getElementById('bet-button')
+
+
+const helpDirections = document.getElementById('help-directions')
+const playerCashMessage = document.getElementById('playercash')
 /*-------------------------------- Functions --------------------------------*/
+
+const startOfGame = () => {
+  dealerCard1.style.display = 'none'
+  dealerCard2.style.display = 'none'
+
+  playerCard1.style.display = 'none'
+  playerCard2.style.display = 'none'
+
+  playerActionButtons.style.display = 'none'
+  playerSubmittedBetValue.style.display = 'none'
+  playerBetButton.style.display = 'none'
+
+  helpDirections.style.display = 'none'
+
+}
+
+startOfGame()
 
 const init = () => {
   deck1 = ["dA", "dQ", "dK", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02", "hA", "hQ", "hK", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02", "cA", "cQ", "cK", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02", "sA", "sQ", "sK", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"]
@@ -39,25 +74,25 @@ const blackjackWinLogic = () => {
   document.getElementById('dealer-card').classList.remove('back-blue', 'shadow', 'outline');
 }
 
-const dealerBlackJackLogic= () => {
+const dealerBlackJackLogic = () => {
 
   message.innerHTML = `Dealer has 21. You lose $$$$`
   document.querySelector('#hit-button').style.display = 'none'
   document.querySelector('#stand-button').style.display = 'none'
 }
 
-const winLogic =() => {
-  if(playerCount > 21) {
+const winLogic = () => {
+  if (playerCount > 21) {
     message.innerHTML = `Player bust... You lose $$$$$`
     document.getElementById('dealer-card').classList.remove('back-blue', 'shadow', 'outline');
-    
+
   }
 
-  if(playerCount === dealerCount) {
+  if (playerCount === dealerCount) {
     message.innerHTML = `Player and Dealer have the same count. You tie!`
   }
 
-  if(dealerCount > playerCount) {
+  if (dealerCount > playerCount) {
     message.innerHTML = `Dealer has ${dealerCount} and Player has ${playerCount}. You lose $$$$`
   }
 }
@@ -68,24 +103,24 @@ const playerDistributeCard = (evt) => {
   let dealerShowingCard = randomizeCard();
   let playerCard2 = randomizeCard();
   let dealerHiddenCard = randomizeCard();
-  
+
   playerCount += getCardValue(playerCard1)
   playerAces += checkForAces(playerCard1)
   document.getElementById('player-card-1').classList.add(playerCard1);
-  
+
   dealerCount += getCardValue(dealerShowingCard)
   dealerAces += checkForAces(dealerShowingCard)
   document.getElementById('dealer-showing-card').classList.add(dealerShowingCard);
-  
+
   playerCount += getCardValue(playerCard2)
   playerAces += checkForAces(playerCard2)
   document.getElementById('player-card-2').classList.add(playerCard2);
-  
+
   dealerCount += getCardValue(dealerHiddenCard)
   dealerAces += checkForAces(dealerHiddenCard)
   document.getElementById('dealer-card').classList.add(dealerHiddenCard, 'large');
 
-  if(playerCount === 21) {
+  if (playerCount === 21) {
     document.querySelector('#hit-button').style.display = 'none'
     document.querySelector('#stand-button').style.display = 'none'
     blackjackWinLogic();
@@ -121,9 +156,9 @@ const playerStandButton = () => {
 const dealerDistributeCard = () => {
   document.getElementById('dealer-card').classList.remove('back-blue', 'shadow', 'outline');
 
-  if(playerCount > 21) {
+  if (playerCount > 21) {
     winLogic();
-  } else if(dealerCount ===21) {
+  } else if (dealerCount === 21) {
     dealerBlackJackLogic();
   } else while (dealerCount < 17) {
     let dealerHitCard = randomizeCard()
@@ -162,12 +197,42 @@ const checkForAces = (card) => {
   } return 0;
 }
 
+const betOption = () => {
+  document.getElementById('start-button').style.display = 'none'
+  playerSubmittedBetValue.style.display = 'block'
+  playerBetButton.style.display = 'block'
+}
+
+const placeBet = () => {
+  playerBet = Number(document.getElementById('bet-placeholder').value)
+  playerCash = playerCash - playerBet
+  console.log(playerBet);
+  console.log(playerCash)
+  document.getElementById('player-bet-value').innerHTML = `Player Bet: $${playerBet}`
+
+}
+
+const showPlayerDealerCards = () => {
+  dealerCard1.style.display = 'block'
+  dealerCard2.style.display = 'block'
+
+  playerCard1.style.display = 'block'
+  playerCard2.style.display = 'block'
+
+  playerActionButtons.style.display = 'block'
+  playerSubmittedBetValue.style.display = 'none'
+  playerBetButton.style.display = 'none'
+
+}
+
 const handleClick = (evt) => {
   const button = evt.target.id
   if (button === 'start-button') {
-
+    betOption();
   }
   if (button === 'bet-button') {
+    placeBet();
+    showPlayerDealerCards()
     randomizeCard();
     playerDistributeCard(evt);
   }
@@ -177,6 +242,8 @@ const handleClick = (evt) => {
   if (button === 'stand-button') {
     playerStandButton();
   }
+  document.getElementById('player-cash').innerHTML = `Player Cash: $${playerCash}`
+
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
